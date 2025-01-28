89aa527b6c485d0699c9505b59746cef53f79ef7
supercoder382001 <63428439+supercoder382001@users.noreply.github.com>
2025-01-28 03:03:03 +0000

    Update testa.js

--- pages/testa.js
+++ pages/testa.js
@@ -46,6 +46,11 @@ export default function Page() {
 
   return (
     <div>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta charSet="UTF-8" />
+        <title>Redirecting</title>
+      </Head>
       <style>{`
         body {
           margin: 0;
